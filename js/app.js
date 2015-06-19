7b35ab881478d2f19087df0e1ba9d3a1f7bee500
Jeffrey Grinberg <jeffgrinberg@Jeffrey-Grinbergs-MacBook.local>
2015-06-19 01:26:03 -0400

    fixed explanation typos

--- js/app.js
+++ js/app.js
@@ -4,22 +4,21 @@ $(document).ready(function() {
 });
 
 /* APPROACH TO SOLVE */
-/* Prioritize the if statements that the first if statement
+/* Prioritize the if statements so that the first if statement
 is the divisible by 3 AND divisible by 5. This is because 
 if I started with just divisible by 3, it'll print for that and
-then it'll also print AGAIN on top. For example, if my code has
-check if it is divisible by 3, then if it is divisible by 5 and
-finally divisible by both....the program when it reaches variable
-equals 15, will have printed Fizz, Buzz AND FizzBuzz. This is
-because the variable satisfied the order or priority of if
+then it'll also print AGAIN on top. For example, if my code has to
+check if it is divisible by 3, it will then check if it is divisible by 5 and
+finally divisible by both....the program when it reaches variable=15, will have printed Fizz, Buzz AND FizzBuzz for variable=15. This is
+because the variable satisfied the order of priority of if
 statements. I only want to print FizzBuzz when it hits 15. In
 order to do this, I need to set the first condition it checks for,
 which is the top priority, to check for if it is divisible by 3
 and 5. Therefore, if it is not divisible by both, it'll simply
 print only fizz or buzz..... When it reaches 15, since it first
-checks for being divisible by both, itll print Fizzbuzz. It will
-then skip the other conditions to check for due to the condition
-having been met. */
+checks for being divisible by both, it'll print Fizzbuzz. It
+will then skip the other conditions to check for due to the
+first condition having been met. */
 
 /*ALSO CANNOT JUST PRINT THE NUMBER AFTER EVERY ITERATION
 ---> Need to set an if condition to check that the number
